758a533735621767b097e22053c1a029acd66af6
Homero Gomez <homerog911@gmail.com>
2023-03-06 23:23:27 +0000

    Completed Lab Instantiation

---
+++ src/main/java/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String color;
+    int numDoors;
+    int numSeats;
+    
+    public Car(String strColor, int intNumDoors, int intNumSeats){
+        color = strColor;
+        numDoors = intNumDoors;
+        numSeats = intNumSeats;
+    }
+}
--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Car car1 = new Car("Blue",5,5);
+        return car1;
     }
 }
